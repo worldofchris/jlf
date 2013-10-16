90faf1af14d100d811ed97b38996c0ed20617395
Chris Young <chris.young@base79.com>
2013-10-16 23:06:41 +0100

    JLF-7: Forgot to add new deps to setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,22 @@ setup(
     include_package_data=True,
     scripts=['bin/jlf'],
     install_requires=[
-        'python-dateutil==1.5',
-        'numpy',
-        'pandas',
-        'requests',
+        'argparse==1.2.1',
+        'ipython==0.13.2',
         'jira-python',
-        'mockito',
-        'xlwt',
-        'argparse'
+        'mockito==0.5.1',
+        'numpy==1.7.1',
+        'oauthlib==0.4.0',
+        'openpyxl==1.6.2',
+        'pandas==0.11.0',
+        'python-dateutil==1.5',
+        'pytz==2013b',
+        'requests==1.2.0',
+        'requests-oauthlib==0.3.1',
+        'six==1.3.0',
+        'tlslite==0.4.1',
+        'wsgiref==0.1.2',
+        'xlrd==0.9.2',
+        'xlwt==0.7.5'        
     ]
 )
