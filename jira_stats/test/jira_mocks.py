3dbeae343889ff601870ae210019a594c57998b0
Chris Young <chris.young@base79.com>
2013-10-16 22:55:14 +0100

    JLF-7: Forgot to add the new tests...

---
+++ jira_stats/test/jira_mocks.py
@@ -0,0 +1,17 @@
+class mockHistory(object):
+
+    def __init__(self, created, items):
+        self.created = created
+        self.items = items
+
+
+class mockItem(object):
+
+    def __init__(self, field, fromString, toString):
+        self.field = field
+        self.fromString = fromString
+        self.toString = toString
+
+START_STATE = 'In Progress'
+END_STATE = 'Customer Approval'
+REOPENED_STATE = 'Reopened'
